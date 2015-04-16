da0c8d0a2de8577a8bb377e14d2ee1d501cc264c
Takafumi Yoshida <zephiransas@gmail.com>
2015-04-16 15:39:20 +0900

    Specify base branch name by options

--- lib/octocam/client.rb
+++ lib/octocam/client.rb
@@ -16,7 +16,7 @@ class Octocam::Client
     Octokit.auto_paginate = true
     client = Octokit::Client.new(access_token: ENV['OCTOCAM_GITHUB_TOKEN'])
 
-    pull_requests = client.pull_requests("#{options[:owner]}/#{options[:repository]}", state: 'closed').reject{|pr| pr[:merged_at] == nil }
+    pull_requests = client.pull_requests("#{options[:owner]}/#{options[:repository]}", state: 'closed', base: options[:base]).reject{|pr| pr[:merged_at] == nil }
     pull_requests.select{|pr| pr[:merged_at].localtime >= options[:from] && pr[:merged_at].localtime <= options[:to] }.sort{|a, b| -(a[:merged_at] <=> b[:merged_at]) }.each do |pull_request|
       puts "- #{pull_request[:title]} [[##{pull_request[:number]}](#{pull_request[:html_url]})]"
     end
